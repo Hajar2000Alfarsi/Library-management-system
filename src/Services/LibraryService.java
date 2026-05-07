f78ecabfaabb699adc8b6dd81f13b485449c8d6a
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 11:17:02 +0400

    Initialize library details

--- src/Services/LibraryService.java
+++ src/Services/LibraryService.java
@@ -5,4 +5,11 @@ import Entites.Library;
 public class LibraryService {
     Library library = new Library();
 
+    public LibraryService() {
+        library.setLibraryName("Evergreen Knowledge Library");
+
+        library.setAboutLibrary("Modern library for learning and community");
+
+        library.setLibraryAdress("Al Khoudh, Muscat, Oman");
+    }
 }
