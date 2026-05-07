6ce5b7660f1a939cd95824cf9baf6e82bbcf9a98
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 10:58:54 +0400

    Set getter and setter

--- src/Entites/LibraryItem.java
+++ src/Entites/LibraryItem.java
@@ -1,5 +1,33 @@
 package Entites;
 
-public class LibraryItem {
+import java.util.UUID;
 
+public class LibraryItem {
+    private UUID id;
+    private String name;
+    private boolean isAvailable = true;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
 }
