6ae4d3f613f0a65269b826007f2f8ba07986bc9d
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 10:24:41 +0400

    Getter and setter

--- src/Entites/Library.java
+++ src/Entites/Library.java
@@ -5,4 +5,29 @@ public class Library {
     private String aboutLibrary;
     private String libraryAdress;
 
+    //getter and setter
+
+    public String getLibraryName() {
+        return libraryName;
+    }
+
+    public void setLibraryName(String libraryName) {
+        this.libraryName = libraryName;
+    }
+
+    public String getAboutLibrary() {
+        return aboutLibrary;
+    }
+
+    public void setAboutLibrary(String aboutLibrary) {
+        this.aboutLibrary = aboutLibrary;
+    }
+
+    public String getLibraryAdress() {
+        return libraryAdress;
+    }
+
+    public void setLibraryAdress(String libraryAdress) {
+        this.libraryAdress = libraryAdress;
+    }
 }
