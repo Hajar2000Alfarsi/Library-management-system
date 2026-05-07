a8d91c7f88dda8bef8b30d86b4787cce59f292d5
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 11:07:28 +0400

    Set getter and setter

--- src/Entites/Magazine.java
+++ src/Entites/Magazine.java
@@ -1,5 +1,13 @@
 package Entites;
 
 public class Magazine {
+    private int issueNumber;
 
+    public int getIssueNumber() {
+        return issueNumber;
+    }
+
+    public void setIssueNumber(int issueNumber) {
+        this.issueNumber = issueNumber;
+    }
 }
