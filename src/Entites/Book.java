085744213d9471a752c1aa290634e072378a068f
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 11:04:38 +0400

    Set getter and setter

--- src/Entites/Book.java
+++ src/Entites/Book.java
@@ -1,5 +1,13 @@
 package Entites;
 
 public class Book extends LibraryItem{
+    private String author;
 
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
