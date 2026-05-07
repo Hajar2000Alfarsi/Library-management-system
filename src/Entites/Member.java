159822d58da0e430be93fbb82258b185dd42a27d
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 11:11:01 +0400

    Set getter and setter

--- src/Entites/Member.java
+++ src/Entites/Member.java
@@ -3,4 +3,31 @@ package Entites;
 import java.util.UUID;
 
 public class Member {
+    private UUID id;
+    private String name;
+    private Address address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
